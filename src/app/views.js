9fb84754497ca8023c6e2486b11784cc261c18fc
Pau Fracés <pfraces@gmail.com>
2014-10-30 05:05:42 +0100

    fixed last commit
    *   added popup event handlers
    *   added damage to weapon builder

--- src/app/views.js
+++ src/app/views.js
@@ -5,41 +5,72 @@ define('app.views', function (require) {
       domo    = require('domo');
 
   var dom = domo.use({
+    hasClass:    require('domo.hasClass'),
     addClass:    require('domo.addClass'),
     removeClass: require('domo.removeClass'),
     on:          require('domo.on')
   });
 
-  var setView = function (view) {    
-    dom('.tab').removeClass('selected');
-    dom('#tab-' + view).addClass('selected');
+  var view = function (name) {    
+    // HACK: determine if there is a popup visible
+    var isBackground = dom('.popup-tab.selected').hasClass('selected') === true;
 
+    if (isBackground) {
+      dom('.view-tab.background').removeClass('background');
+      dom('#tab-' + name).addClass('background');
+    } else {
+      dom('.view-tab.selected').removeClass('selected');
+      dom('#tab-' + name).addClass('selected');
+    }
 
-    dom('.view').removeClass('visible');
-    dom('#' + view).addClass('visible');
+    dom('.view.visible').removeClass('visible');
+    dom('#' + name).addClass('visible');
 
-    dom('.view-panel').removeClass('visible');
-    dom('#panel-' + view).addClass('visible');
+    dom('.view-panel.visible').removeClass('visible');
+    dom('#panel-' + name).addClass('visible');
   };
 
-  var enableViewTab = function (view) {
-    dom('#tab-' + view)
+  var popup = function (name) {
+    var isSelected = dom('#tab-' + name).hasClass('selected');
+
+    dom('.popup-tab.selected').removeClass('selected');
+    dom('.popup.visible').removeClass('visible');
+    
+    if (isSelected) {
+      dom('.view-tab.background')
+      .removeClass('background').addClass('selected');
+      
+      return;
+    }
+
+    dom('.view-tab.selected')
+    .removeClass('selected').addClass('background');
+    
+    dom('#tab-' + name).addClass('selected');
+    dom('#' + name).addClass('visible');
+  };
+
+  var enableView = function (name) {
+    var tab = dom('#tab-' +  name),
+        isPopup = tab.hasClass('popup-tab'),
+        handler = isPopup ? popup : view;
+
+    tab
     .addClass('visible')
-    .on('click', partial(setView, view)); 
+    .on('click', partial(handler, name)); 
   };
 
-  var disableViewTab = function (view) {
-    dom('#show-' + view)
-    .removeClass('visible');
+  var disableView = function (view) {
+    dom('#tab-' + view).removeClass('visible');
   };
 
   var init = function () {
-    setView('home');
+    view('home');
   };
 
   return {
-    enable: enableViewTab,
-    disable: disableViewTab,
+    enable: enableView,
+    disable: disableView,
     init: init
   };
 });
