6e22c7c4b0350a7179053748a46d82f0e282d3d8
Pau Fracés <pfraces@gmail.com>
2014-11-17 01:44:08 +0100

    added signup and login to firebase

--- src/views/login/login.js
+++ src/views/login/login.js
@@ -1,17 +1,58 @@
 define('app.login', function (require) {
   'use strict';
 
-  var tab = require('ui.tab');
+  var storage = require('storage'),
+      tab     = require('ui.tab');
 
   var dom =      require('domo').use({
     addClass:    require('domo.addClass'),
-    removeClass: require('domo.removeClass')
+    removeClass: require('domo.removeClass'),
+    on:          require('domo.on'),
+    val:         require('domo.val')
   });
 
   var init = function () {
     tab.group('login', function (contentId) {
       dom('.login.section.visible').removeClass('visible');
       dom('#' + contentId).addClass('visible');
+    });
+
+    dom('#login-login-submit').on('click', function () {
+      var user = {
+        email: dom('#login-login-email').val(),
+        password: dom('#login-login-password').val()
+      };
+
+      storage.authWithPassword(user, function (err, auth) {
+        if (err) { throw err; }
+        console.log('user', auth);
+      });
+    });
+
+    dom('#login-signup-submit').on('click', function () {
+      var user = {
+        name: dom('#login-signup-user').val(),
+        email: dom('#login-signup-email').val(),
+        password: dom('#login-signup-password').val()
+      };
+
+      storage.createUser(user, function (err) {
+        if (err) { throw err; }
+
+        // auto login
+
+        storage.authWithPassword(user, function (err, auth) {
+          if (err) { throw err; }
+
+          var profile = {
+            name: user.name,
+            uid: auth.uid,
+            email: auth.password.email
+          };
+
+          storage.child('users').child(profile.uid).set(profile);
+        });
+      });
     });
   };
 
